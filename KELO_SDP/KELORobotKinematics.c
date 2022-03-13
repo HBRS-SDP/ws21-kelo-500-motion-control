3c7edfb0a6c2fd14b621ec9b15d0d23e1d57b4da
Kishan Sawant <kishansawant96@gmail.com>
2022-03-13 15:52:18 +0100

    refactoring of code by adding docstrings

--- KELO_SDP/KELORobotKinematics.c
+++ KELO_SDP/KELORobotKinematics.c
@@ -1,3 +1,10 @@
+/**
+ * @file KELORobotKinematics.c
+ * @author Kishan Sawant (kishan.sawant@smail.inf.h-brs.de)
+ * @brief function to calculate the 3x8 Jacobian matrix(A) and to calculate forces at individual wheel units
+ * @date 2022-03-12
+ * 
+ */
 #include <stdio.h>
 #include <gsl/gsl_sf_trig.h>
 #include <gsl/gsl_linalg.h>
@@ -34,37 +41,56 @@ void force_vector_finder(double *pivot_forces,
                          const gsl_matrix *b,
                          const unsigned int M)
 {
-    /* 
-    Equations:
+    /**
+     * @brief Equations:
     J# = Mq * V * pinv_dls(S) * U.T * Mx
     B  = Mx * J * Mq (Note: B = U * S * V.T)
     J# * b = pivot_forces
-    */
+     * 
+     */
 
-    // B  = Mx * J * Mq
+    /**
+     * @brief B  = Mx * J * Mq
+     * 
+     */
     gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, W, A, 0.0, A_tmp);
     gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, A_tmp, K, 0.0, A);
     gsl_matrix_transpose_memcpy(A_inv_T, A); // we need M>N to solve SVD, but as M < N, transpose of A has been considered
-    // Note: singular values are invariant to matrix transpose, as S = S.T
+    /**
+     * @brief Note: singular values are invariant to matrix transpose, as S = S.T
+     * 
+     */
      
-    // B = U * S * V.T 
+    /**
+     * @brief B = U * S * V.T
+     * 
+     */
     gsl_linalg_SV_decomp(A_inv_T, V, u, work); // SVD decomposition (A gets replaced by values of U matrix)
     gsl_matrix_transpose(V); // here V is indeed U, as transpose of A is being considered
 
-    // finding S_inv
+    /**
+     * @brief finding S_inv
+     * 
+     */
     size_t i;
     for (i = 0; i < u->size; i++)
     {
         gsl_matrix_set(u_inv, i, i, 1/gsl_vector_get(u,i));
     }
 
-    // J# = M_q * V * pinv_dls(S) * U.T * M_x
+    /**
+     * @brief J# = M_q * V * pinv_dls(S) * U.T * M_x
+     * 
+     */
     gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, K, A_inv_T, 0.0, A_inv_T_tmp); 
     gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, A_inv_T_tmp, u_inv, 0.0, A_inv_T);
     gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, A_inv_T, V, 0.0, A_inv_T_tmp);
     gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, A_inv_T_tmp, W, 0.0, A_inv_T);
     gsl_matrix_view _X = gsl_matrix_view_array(pivot_forces,M,1);
 
-    // J# * b = pivot_forces
+    /**
+     * @brief J# * b = pivot_forces
+     * 
+     */
     gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, A_inv_T, b, 0.0, &_X.matrix);
 }
--- KELO_SDP/KELORobotKinematics.h
+++ KELO_SDP/KELORobotKinematics.h
@@ -1,25 +1,55 @@
+/**
+ * @file KELORobotKinematics.h
+ * @author your name (you@domain.com)
+ * @brief function to calculate the 3x8 Jacobian matrix(A) and to calculate forces at individual wheel units
+ * @date 2022-03-12
+ * 
+ */
 #ifndef KELO_ROBOT_KINEMATICS_H
 #define KELO_ROBOT_KINEMATICS_H
 
 #include <gsl/gsl_matrix_double.h>
 
+/**
+ * @brief function to calculate the 3x8 matrix(A) to map force from individual wheels to platform
+ * 
+ * @param A 3x8 matrix(Jacobian) which maps form individual wheels to platform
+ * @param pivot_angles 1D array of shape (4,) that stores current pivot angles of individual pair of wheels
+ * @param wheel_coordinates 1-D array of shape (8,) which holds the co-ordinates of individual wheels (x1,y1,x2,y2,..,y4)
+ */
 void jacobian_matrix_calculator(gsl_matrix *A,
                                 const double *pivot_angles,
                                 const double *wheel_coordinates);
 
-
+/**
+ * @brief to solve the inverse of the Jacobian matrix(A) and calculate the pivot forces using it
+ * 
+ * @param pivot_forces forces at individual wheel units
+ * @param A Jacobian matrix of shape 3x8, which is further used to store W.dot(A).dot(K)
+ * @param A_tmp a temporary matrix used to store the matrix multiplication of W.dot(A)
+ * @param A_inv_T_tmp used as temporary memory to calculate A_inv_T
+ * @param A_inv_T initially store left singular matrix of SVD decomposition of A and later it stores the inverse of W.dot(A).dot(K)
+ * @param u a vector consisting of singular values of Sigma matrix in SVD decomposition of A(Jacobian matrix)
+ * @param u_inv inverse of the diagonal matrix consisting of eigenvalues of Sigma matrix
+ * @param V right singular matrix of SVD decomposition
+ * @param W weight matrix of shape 3x3
+ * @param K weight matrix of shape 8x8
+ * @param work temporary memory to facilitate SVD of Jacobian matrix(A)
+ * @param b platform force matrix of shape 3x1
+ * @param M number of wheel units (8) 
+ */
 void force_vector_finder(double *pivot_forces,
-                        gsl_matrix *A,
-                        gsl_matrix *A_prime_tmp,
-                        gsl_matrix *A_prime_tmp_T,
-                        gsl_matrix *A_prime,
-                        gsl_vector *u,
-                        gsl_matrix *u_inv,
-                        gsl_matrix *V,
-                        const gsl_matrix *W,
-                        const gsl_matrix *K,
-                        gsl_vector *work,
-                        const gsl_matrix *b,
-                        const unsigned int M);
+                         gsl_matrix *A,
+                         gsl_matrix *A_tmp,
+                         gsl_matrix *A_inv_T_tmp,
+                         gsl_matrix *A_inv_T,
+                         gsl_vector *u,
+                         gsl_matrix *u_inv,
+                         gsl_matrix *V,
+                         const gsl_matrix *W,
+                         const gsl_matrix *K,
+                         gsl_vector *work,
+                         const gsl_matrix *b,
+                         const unsigned int M);
 
 #endif
