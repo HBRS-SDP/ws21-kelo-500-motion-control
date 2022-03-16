4b3afe4fb681595538359a5b67869f5afd365dc2
Rahul Sai <38484825+rahulsai1219@users.noreply.github.com>
2022-03-16 19:10:25 +0100

    Variable name change

--- KELO_SDP/SmartWheelKinematics.c
+++ KELO_SDP/SmartWheelKinematics.c
@@ -11,6 +11,8 @@
 
 #define LEFT_WHEEL 0
 #define RIGHT_WHEEL 1
+#define X_COMPONENT 0
+#define Y_COMPONENT 1
 
 void map_pivot_force_to_wheel_torque(const double *pivot_forces,
                                      double *wheel_torques,
@@ -18,8 +20,8 @@ void map_pivot_force_to_wheel_torque(const double *pivot_forces,
                                      const double castor_offset,
                                      const double half_wheel_distance)
 {
-    wheel_torques[LEFT_WHEEL] = (radius / 2) * (pivot_forces[RIGHT_WHEEL] / (castor_offset / half_wheel_distance) + pivot_forces[LEFT_WHEEL]);
-    wheel_torques[RIGHT_WHEEL] = (radius / 2) * (-pivot_forces[RIGHT_WHEEL] / (castor_offset / half_wheel_distance) + pivot_forces[LEFT_WHEEL]);
+    wheel_torques[LEFT_WHEEL] = (radius / 2) * (pivot_forces[Y_COMPONENT] / (castor_offset / half_wheel_distance) + pivot_forces[X_COMPONENT]);
+    wheel_torques[RIGHT_WHEEL] = (radius / 2) * (-pivot_forces[Y_COMPONENT] / (castor_offset / half_wheel_distance) + pivot_forces[X_COMPONENT]);
 }
 
 void map_pivot_forces_to_wheel_torques(const double *pivot_forces,
