bde6a47e341ff2a5a5e126b7d1acae3396ad15a0
KishanSawant <40161007+KishanSawant@users.noreply.github.com>
2022-05-17 15:43:12 +0200

    Correction in force to torque conversion formula

--- KELO_SDP/SmartWheelKinematics.c
+++ KELO_SDP/SmartWheelKinematics.c
@@ -20,8 +20,8 @@ void map_pivot_force_to_wheel_torque(const double *pivot_forces,
                                      const double castor_offset,
                                      const double half_wheel_distance)
 {
-    wheel_torques[LEFT_WHEEL] = (radius / 2) * (pivot_forces[Y_COMPONENT] / (castor_offset / half_wheel_distance) + pivot_forces[X_COMPONENT]);
-    wheel_torques[RIGHT_WHEEL] = (radius / 2) * (-pivot_forces[Y_COMPONENT] / (castor_offset / half_wheel_distance) + pivot_forces[X_COMPONENT]);
+    wheel_torques[LEFT_WHEEL] = (radius / 2) * (pivot_forces[Y_COMPONENT] * (castor_offset / half_wheel_distance) + pivot_forces[X_COMPONENT]);
+    wheel_torques[RIGHT_WHEEL] = (radius / 2) * (-pivot_forces[Y_COMPONENT] * (castor_offset / half_wheel_distance) + pivot_forces[X_COMPONENT]);
 }
 
 void map_pivot_forces_to_wheel_torques(const double *pivot_forces,
